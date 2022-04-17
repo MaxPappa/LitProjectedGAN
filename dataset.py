18ca72f48b1ac1890a8fe4652a33df93b5761938
MaxPappa <massimilianopappa95@gmail.com>
2022-04-17 13:10:25 +0200

    dataset loader using imagefolder. Images are normalized in an interval [-1,1].

---
+++ dataset.py
@@ -0,0 +1,12 @@
+import torch
+from torchvision import transforms, datasets
+
+
+def load_data(data_path, batch_size):#, mean, std):
+    train_transforms = transforms.Compose([transforms.Resize((256, 256)),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])])
+    train_data = datasets.ImageFolder(data_path, transform=train_transforms)
+    trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8)
+    return trainloader
